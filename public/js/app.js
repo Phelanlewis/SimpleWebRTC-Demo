41981a1d1456bef3822cdfcd5f2b2724a488f77b
Phelan Lewis <phelanlewis@gmail.com>
2018-05-30 22:48:42 -0700

    added chat room functionality with messages

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,86 @@
 window.addEventListener('load', () => {
   // Put all client-side code here
+  
+//add event handler for the join/create room button
+$('.submit').on('click', (event) => {
+  if (!formEl.form('is valid')) {
+    return false;
+  }
+  username = $('#username').val();
+  const roomName = $('#roomName').val().toLowerCase();
+  if (event.target.id === 'create-btn') {
+    createRoom(roomName);
+  } else {
+    joinRoom(roomName);
+  }
+  return false;
+});
+
+// Register new Chat Room
+const createRoom = (roomName) => {
+  console.info(`Creating new room: ${roomName}`);
+  webrtc.createRoom(roomName, (err, name) => {
+    showChatRoom(name);
+    postMessage(`${username} created chatroom`);
+  });
+};
+
+// Join existing Chat Room
+const joinRoom = (roomName) => {
+  console.log(`Joining Room: ${roomName}`);
+  webrtc.joinRoom(roomName);
+  showChatRoom(roomName);
+  postMessage(`${username} joined chatroom`);
+};
+
+// Post Local Message
+const postMessage = (message) => {
+  const chatMessage = {
+    username,
+    message,
+    postedOn: new Date().toLocaleString('en-GB'),
+  };
+  // Send to all peers
+  webrtc.sendToAll('chat', chatMessage);
+  // Update messages locally
+  messages.push(chatMessage);
+  $('#post-message').val('');
+  updateChatMessages();
+};
+
+// Display Chat Interface
+const showChatRoom = (room) => {
+  // Hide form
+  formEl.hide();
+  const html = chatTemplate({ room });
+  chatEl.html(html);
+  const postForm = $('form');
+  // Post Message Validation Rules
+  postForm.form({
+    message: 'empty',
+  });
+  $('#post-btn').on('click', () => {
+    const message = $('#post-message').val();
+    postMessage(message);
+  });
+  $('#post-message').on('keyup', (event) => {
+    if (event.keyCode === 13) {
+      const message = $('#post-message').val();
+      postMessage(message);
+    }
+  });
+};
+
+// Update Chat Messages
+const updateChatMessages = () => {
+  const html = chatContentTemplate({ messages });
+  const chatContentEl = $('#chat-content');
+  chatContentEl.html(html);
+  // automatically scroll downwards
+  const scrollHeight = chatContentEl.prop('scrollHeight');
+  chatContentEl.animate({ scrollTop: scrollHeight }, 'slow');
+};
+
 });
 
 // Chat platform
